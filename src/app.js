257f4a9ade3ec511981c4d8291738c92c8529e70
Nicolas Hoiak <nicohoiak@gmail.com>
2023-11-07 17:04:32 -0300

    arreglo 2.0

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import express from 'express'
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))}
+
+const PORT = 8080
 
 app.listen(PORT, () => {
     console.log(`Primer backend activo en puerto ${PORT}`)
