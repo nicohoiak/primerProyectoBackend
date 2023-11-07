f7e3eead56e08b4e9549b54a7e1c80e7397df388
Nicolas Hoiak <nicohoiak@gmail.com>
2023-11-07 17:05:49 -0300

    arreglo 2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express'
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))}
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = 8080
 
