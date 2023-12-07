dd5caaba8f741515fad4d8b5bdeac187797925e2
Nicolas Hoiak <nicohoiak@gmail.com>
2023-12-07 19:03:52 -0300

    arreglo segunda entrega 2.0

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { CartManager } from './cartManager.js' */
 import viewsRouter from './routes/views.routes.js'
 import productsRouter from './routes/products.routes.js'
 import cartsRouter from './routes/carts.routes.js'
+import ordersRouter from './routes/orders.routes.js'
 
 const PORT = 8080
 const MONGOOSE_URL = 'mongodb://localhost:27017'
@@ -25,7 +26,7 @@ export const cartManager = new CartManager */
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 app.use('/', viewsRouter)
-app.use('/api/products', productsRouter)
+app.use('/api/orders', ordersRouter)
 
 app.use('/static', express.static(`${__dirname}/public`))
 
